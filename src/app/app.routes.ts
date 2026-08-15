0c5118bb4205e1ca9384e350b38aefa5bbe141e4
Anna Seby <anna.seby@griffithuni.edu.au>
2026-08-15 21:08:30 +1000

    Final Workshop 5 routing and test cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,9 @@ import { Login } from './login/login';
 import { Profile } from './profile/profile';
 
 export const routes: Routes = [
-	{ path: '', pathMatch: 'full', redirectTo: 'home' },
+	{ path: '', pathMatch: 'full', redirectTo: 'login' },
 	{ path: 'login', component: Login },
 	{ path: 'home', component: Home, canActivate: [authGuard] },
 	{ path: 'profile', component: Profile, canActivate: [authGuard] },
-	{ path: '**', redirectTo: 'home' },
+	{ path: '**', redirectTo: 'login' },
 ];
